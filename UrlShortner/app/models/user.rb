ef5d3433a3b2300129c7711cbcda998f82a48721
Phanindra Srungavarapu <pahninsd@gmail.com>
2018-04-12 11:00:20 +0530

    Setting up Device and omniauth

---
+++ UrlShortner/app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :omniauthable, omniauth_providers: %i[google_oauth2]
+end
